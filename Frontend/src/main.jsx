e4b079f872d4d11799573f3725e9dfd24ef80d64
Kálóczi Kornél <kornel580@gmail.com>
2023-11-16 01:09:04 +0100

    add some basic theme override
    WIP kind of things

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
+import { BrowserRouter } from 'react-router-dom';
+import { Router } from './Router.jsx';
+import { responsiveFontSizes, ThemeProvider } from '@mui/material';
+import constructTheme from '../theme.js';
+
+let theme = constructTheme();
+theme = responsiveFontSizes(theme);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <App />
+        <BrowserRouter>
+            <ThemeProvider theme={theme}>
+                <Router />
+            </ThemeProvider>
+        </BrowserRouter>
     </React.StrictMode>
 );
