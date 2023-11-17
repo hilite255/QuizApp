ecccf70654d7fae2bc5af88954f4a4d1686a866b
Kálóczi Kornél <kornel580@gmail.com>
2023-11-17 11:38:38 +0100

    add button for navigation

--- Frontend/src/pages/HomePage.jsx
+++ Frontend/src/pages/HomePage.jsx
@@ -1,5 +1,14 @@
-import { Typography } from '@mui/material';
+import { Button, Typography } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 
 export const HomePage = () => {
-    return <Typography>asdfasfa</Typography>;
+    const navigate = useNavigate();
+    return (
+        <>
+            <Typography>asdfasfa</Typography>
+            <Button onClick={() => navigate('/quiz/1')}>
+                Move to the quiz page
+            </Button>
+        </>
+    );
 };
