1e2e785ac16ce8b2864dc8bce6ccb27ef8e8820e
Kálóczi Kornél <kornel580@gmail.com>
2023-11-17 11:38:20 +0100

    adding quiz page route (for filling in the quiz)

--- Frontend/src/Router.jsx
+++ Frontend/src/Router.jsx
@@ -3,6 +3,8 @@ import { Routes, Route } from 'react-router-dom';
 import { Container } from '@mui/material';
 import { HomePage } from './pages/HomePage.jsx';
 import { MenuBar } from './components/MenuBar.jsx';
+import { QuizPage } from './pages/QuizPage.jsx';
+import { QuizPageContainer } from './containers/QuizPageContainer.jsx';
 
 export const Router = () => {
     return (
@@ -11,6 +13,11 @@ export const Router = () => {
             <Container>
                 <Routes>
                     <Route path="/" exact element={<HomePage />} />
+                    <Route
+                        path="/quiz/:id"
+                        exact
+                        element={<QuizPageContainer />}
+                    />
                 </Routes>
             </Container>
         </>
