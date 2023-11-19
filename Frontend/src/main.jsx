ef4486c47ee6e30005d7babd4d7f75270f33e7ce
Kálóczi Kornél <kornel580@gmail.com>
2023-11-19 15:57:25 +0100

    add auth0provider

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -4,6 +4,8 @@ import { BrowserRouter } from 'react-router-dom';
 import { Router } from './Router.jsx';
 import { responsiveFontSizes, ThemeProvider } from '@mui/material';
 import constructTheme from '../theme.js';
+import { Auth0Provider } from '@auth0/auth0-react';
+import('../styles/index.styl');
 
 let theme = constructTheme();
 theme = responsiveFontSizes(theme);
@@ -12,7 +14,15 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <BrowserRouter>
             <ThemeProvider theme={theme}>
-                <Router />
+                <Auth0Provider
+                    domain="dev-1ckx0xgt.us.auth0.com"
+                    clientId="OacUI6sz6PujDRHVGFqMMQ75HegUtKbA"
+                    authorizationParams={{
+                        redirect_uri: window.location.origin
+                    }}
+                >
+                    <Router />
+                </Auth0Provider>
             </ThemeProvider>
         </BrowserRouter>
     </React.StrictMode>
