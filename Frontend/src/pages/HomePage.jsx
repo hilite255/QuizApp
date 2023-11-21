7848243405aa4cf9eb81bf41e7624add8a6d5183
Kálóczi Kornél <kornel580@gmail.com>
2023-11-22 00:03:55 +0100

    add something to the front page WIP

--- Frontend/src/pages/HomePage.jsx
+++ Frontend/src/pages/HomePage.jsx
@@ -1,17 +1,48 @@
-import { Button, Typography } from '@mui/material';
+import { Button, Grid, Typography } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
-import { AXIOS_METHOD, useApi } from '../hooks/useApi.jsx';
+import PetsIcon from '@mui/icons-material/Pets';
+import PersonIcon from '@mui/icons-material/Person';
+import QuizIcon from '@mui/icons-material/Quiz';
+import NoteAddIcon from '@mui/icons-material/NoteAdd';
 
 export const HomePage = () => {
     const navigate = useNavigate();
-    const { data, isLoading, apiCallCallback } = useApi(
-        AXIOS_METHOD.GET,
-        '/api/user/login'
-    );
-
-    console.log(data);
     return (
         <>
+            <Grid container sx={{ marginTop: '24px' }}>
+                <Grid
+                    item
+                    xs={6}
+                    sx={{ display: 'flex', justifyContent: 'end' }}
+                >
+                    <PersonIcon
+                        color="primary"
+                        sx={{ width: '250px', height: '250px' }}
+                    />
+                </Grid>
+                <Grid item xs={6}>
+                    <QuizIcon
+                        color="primary"
+                        sx={{ width: '250px', height: '250px' }}
+                    />
+                </Grid>
+                <Grid
+                    item
+                    xs={6}
+                    sx={{ display: 'flex', justifyContent: 'end' }}
+                >
+                    <NoteAddIcon
+                        color="primary"
+                        sx={{ width: '250px', height: '250px' }}
+                    />
+                </Grid>
+                <Grid item xs={6}>
+                    <PetsIcon
+                        color="primary"
+                        sx={{ width: '250px', height: '250px' }}
+                    />
+                </Grid>
+            </Grid>
             <Typography>asdfasfa</Typography>
             <Button onClick={() => navigate('/quiz/1')}>
                 Move to the quiz page
