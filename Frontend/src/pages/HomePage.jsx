7298837d94d16db9bc73852901477d742abec8de
Kálóczi Kornél <kornel580@gmail.com>
2023-11-21 20:45:29 +0100

    Merge branch 'main' of /Users/kornelkaloczi/Dev/UNI/QuizApp with conflicts.

--- Frontend/src/pages/HomePage.jsx
+++ Frontend/src/pages/HomePage.jsx
@@ -1,14 +1,20 @@
 import { Button, Typography } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
+import { AXIOS_METHOD, useApi } from '../hooks/useApi.jsx';
 
 export const HomePage = () => {
     const navigate = useNavigate();
+    const { data, isLoading, apiCallCallback } = useApi(
+        AXIOS_METHOD.GET,
+        '/api/quiz'
+    );
     return (
         <>
             <Typography>asdfasfa</Typography>
             <Button onClick={() => navigate('/quiz/1')}>
                 Move to the quiz page
             </Button>
+            <Button onClick={() => apiCallCallback()}>test</Button>
         </>
     );
 };
