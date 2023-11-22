d2e653e0cf680737f982f8c74ad9c76414e173be
Kálóczi Kornél <kornel580@gmail.com>
2023-11-22 16:58:04 +0100

    move auth0 things into config

--- Frontend/src/config.js
+++ Frontend/src/config.js
@@ -10,3 +10,9 @@ export const QUESTION_TYPES = {
     MULTIPLE: 'multipleChoice',
     TRUE_FALSE: 'trueFalse'
 };
+
+export const AUTH0_CONFIG = {
+    DOMAIN: 'dev-1ckx0xgt.us.auth0.com',
+    CLIENT_ID: 'OacUI6sz6PujDRHVGFqMMQ75HegUtKbA',
+    AUDIENCE: 'http://quiz-app.hu/'
+};
